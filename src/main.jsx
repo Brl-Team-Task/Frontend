2e9adce8fa48a690cfd89cb26c3cc1453e75d7e2
Lucifer Locas <rohitsomvanshi2003@gmail.com>
2023-11-25 22:16:31 +0530

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Home from './assets/Components/home'
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <>
+    <Home />
+  </>
+)
