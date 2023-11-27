552f0e10fa41ebafa6a5957ef047c326c4d3234f
Lucifer Locas <rohitsomvanshi2003@gmail.com>
2023-11-27 19:46:15 +0530

    fetched attendance data

--- src/Signup.jsx
+++ src/Signup.jsx
@@ -1,21 +1,21 @@
-import React, { useState } from 'react';
-import { useNavigate, Link } from 'react-router-dom';
-import axios from 'axios';
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import './Style.css'
-import './AKGEC.png'
+import React, { useState } from "react";
+import { useNavigate, Link } from "react-router-dom";
+import axios from "axios";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import "./Style.css";
+import "./AKGEC.png";
 
 const Signup = () => {
-  const [userId, setUserId] = useState('');
-  const [password, setPassword] = useState('');
+  const [userId, setUserId] = useState("");
+  const [password, setPassword] = useState("");
   const [showPassword, setShowPassword] = useState(false);
   const navigate = useNavigate(); // Added useNavigate hook
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     if (password.length < 8) {
-      toast.error('Password must be at least 8 characters long');
+      toast.error("Password must be at least 8 characters long");
       return;
     }
     try {
@@ -23,26 +23,28 @@ const Signup = () => {
         user_id: userId,
         password,
       };
-      const response = await axios.post('https://erp-backend-mqly.onrender.com/api/register/', data)
+      const response = await axios.post(
+        "https://erp-backend-mqly.onrender.com/api/register/",
+        data
+      );
       console.log(response.data);
       if (response.status === 201) {
-        toast.success('OTP sent to your email');
+        toast.success("OTP sent to your email");
         // Delay navigation to verify page for 3 seconds
         setTimeout(() => {
-          navigate('/verify'); // Adjust the route as needed
-        }, 3000);
+          navigate("/verify"); // Adjust the route as needed
+        }, 1000);
       } else if (response.status === 400) {
-        toast.error('A valid integer is required')
+        toast.error("A valid integer is required");
       } else {
-        toast.error("Something went wrong")
+        toast.error("Sign-in again");
       }
     } catch (error) {
       if (error.response && error.response.status === 404) {
-        toast.error("Invalid Credentials")
+        toast.error("Invalid Credentials");
       } else {
-        toast.error('Internal Server Error');
+        toast.error("Internal Server Error");
       }
-
     }
   };
 
@@ -51,12 +53,15 @@ const Signup = () => {
   };
 
   return (
-    <div className='Signimage'>
-      <div className='header'>
-        <img src="https://www.akgec.ac.in/wp-content/themes/twentysixteen/img/AKGEC_1_0.png" alt="" />
+    <div className="Signimage">
+      <div className="header">
+        <img
+          src="https://www.akgec.ac.in/wp-content/themes/twentysixteen/img/AKGEC_1_0.png"
+          alt=""
+        />
         <p>Ajay Kumar Garg Engineering College,Ghaziabad</p>
       </div>
-      <div className='SignLayout'>
+      <div className="SignLayout">
         <h3>Login</h3>
         <form onSubmit={handleSubmit}>
           <label>Username</label>
@@ -64,23 +69,38 @@ const Signup = () => {
             type="text"
             value={userId}
             onChange={(e) => setUserId(e.target.value)}
-            required placeholder='username'
+            required
+            placeholder="username"
           />
           <br />
           <label>Password</label>
           <input
-            type={showPassword ? 'text' : 'password'}
+            type={showPassword ? "text" : "password"}
             value={password}
-            required placeholder='password'
+            required
+            placeholder="password"
             onChange={(e) => setPassword(e.target.value)}
           />
-          {showPassword ? <i className="fa fa-regular fa-eye" onClick={handleShowPassword}></i> : <i className="fa-regular fa-eye-slash " onClick={handleShowPassword}></i>}
+          {showPassword ? (
+            <i
+              className="fa fa-regular fa-eye"
+              onClick={handleShowPassword}
+            ></i>
+          ) : (
+            <i
+              className="fa-regular fa-eye-slash "
+              onClick={handleShowPassword}
+            ></i>
+          )}
           <br />
           <Link to={`/Forgot`}>
             <p>Forgot Password?</p>
           </Link>
-          <button className='submit' type="submit">Sign in</button>
-        </form></div>
+          <button className="submit" type="submit">
+            Sign in
+          </button>
+        </form>
+      </div>
       <ToastContainer />
     </div>
   );
--- src/Verify.jsx
+++ src/Verify.jsx
@@ -1,13 +1,16 @@
-import React, { useState } from 'react';
-import axios from 'axios';
-import './Style.css'
-import VerifyImg from '../src/VerifyImg.png'
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import React, { useState } from "react";
+import axios from "axios";
+import "./Style.css";
+import VerifyImg from "../src/VerifyImg.png";
+import { useNavigate } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Attendance from "./attendance";
 
 const Verify = () => {
-  const [email, setEmail] = useState('');
-  const [otp, setOtp] = useState('');
+  const [email, setEmail] = useState("");
+  const [otp, setOtp] = useState("");
+  const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -15,54 +18,69 @@ const Verify = () => {
     const data = { email, otp };
 
     try {
-      const response = await axios.post('https://erp-backend-mqly.onrender.com/api/verifyotp/', data);
-console.log(response.data);
-      if (response.data.status === 200) {
-      toast.success('OTP verified. You can now sign in.');
-      }
-      else if (response.data.status===400){
-        toast.error("Invalid Credentials")
-      }
-      else{
-        toast.error('Something went wrong')
-      }
+      const response = await axios.post(
+        "https://erp-backend-mqly.onrender.com/api/verifyotp/",
+        data
+      );
 
+      console.log(response.data);
+      if (response.data.status === 201) {
+        toast.success("OTP verified! Redirecting ...");
+        const token = response.data.token;
+        localStorage.setItem('Token', token);
+        navigate("/attendance");
+      } else if (response.status === 400) {
+        toast.error("Invalid OTP, try again");
+      } else {
+        toast.error("Something went wrong");
+      }
     } catch (error) {
       console.error(error);
-      alert('Something went wrong. Please try again later.');
+      toast.error(
+        "An error occurred while verifying the OTP. Please try again later."
+      );
     }
   };
 
   return (
-    <div className='Signimage'>
-      <div className='VerifyLayout'>
-      <div className='VerifyImage'>
-          <img src={VerifyImg} alt="" /></div>
-  
-        <div className='Verify'>
-      <h1>OTP Verification</h1>
-      <form onSubmit={handleSubmit}>
-<label>Enter the OTP sent to the mail</label>
+    <>
+      <div className="Signimage">
+        <div className="VerifyLayout">
+          <div className="VerifyImage">
+            <img src={VerifyImg} alt="" />
+          </div>
+          <div className="Verify">
+            <h1>OTP Verification</h1>
+            <form onSubmit={handleSubmit}>
+              <label>Enter the OTP sent to the mail</label>
 
-        
-<input   className='VerifyInput'
-type="email"  
-required  
-value={email} placeholder='Email'
-onChange={(e) => setEmail(e.target.value)}
-        />
-        <br />
-<label>OTP:</label>
-        <input className='VerifyInput'
-          type="text"
-          required
-          value={otp}
-          onChange={(e) => setOtp(e.target.value)}
-        />
-        <br />
-        <button type="submit" className='VerifyButton'>Verify</button>
-      </form></div> </div>  </div>
-     
+              <input
+                className="VerifyInput"
+                type="email"
+                required
+                value={email}
+                placeholder="Email"
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <br />
+              <label>OTP:</label>
+              <input
+                className="VerifyInput"
+                type="text"
+                required
+                value={otp}
+                onChange={(e) => setOtp(e.target.value)}
+              />
+              <br />
+              <button type="submit" className="VerifyButton">
+                Verify
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+      <ToastContainer />
+    </>
   );
 };
 
