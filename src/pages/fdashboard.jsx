b1d9c40eb629dd281badd9ad997af22369048b32
Rishikeshari7 <rishikeshari7@gmail.com>
2023-12-02 02:28:41 +0530

    fetch the data from faculty api

--- src/pages/fdashboard.jsx
+++ src/pages/fdashboard.jsx
@@ -1,49 +1,44 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 function YourComponent() {
-  const [newData, setData] = useState([]);
+  const token = localStorage.getItem("Token");
+  const [dataa, setDataa] = useState([]);
 
-    async function fetchData() {
-      try {
-        const token = localStorage.getItem('Token');
-         console.log(token);
-         
-        if (!token) {
-          console.log("no token value found"); 
-          console.log(token);
-        }
-        const response = await axios.get('https://erp-backend-mqly.onrender.com/api/getfaculty/', 
+ useEffect(()=>{
+  async function fetchFaculty(){
+    try {
+      const response = await axios.get(
+        "https://erp-backend-mqly.onrender.com/api/getfaculty/",
         {
           headers: {
-            token: `Bearer ${token}`,
+            Authorization: `Bearer ${token}`,
           },
-        },{data:{
-          token
-        }}
-        );
-        console.log("response of api->",response);
-        setData(response.data);
-        console.log("data of api",newData);
-      } 
-      catch(error) {
-          console.log('response:', error.response);
-          console.error('error hai:', error);
-      }
+        }
+      );
+      console.log(response.data.faculty_data);
+      setDataa(response.data.faculty_data);
+      console.log(dataa);
+   
+     
+    } catch (error) {
+      console.log("Internal Server Error",error);
     }
+  }
+  fetchFaculty();
+  
 
-    fetchData();
- 
-
+ },[]);
+  
+    
   return (
-    <div>
-      <h1>Faculty</h1>
-      <ul>
-        {newData.map((item) => (
-          <li key={item.id}>{item.name}</li>
-        ))}
-      </ul>
-    </div>
+    <>
+      <div>
+        <h1>Faculty</h1>
+        
+      </div>
+      
+    </>
   );
 }
 
