e57d46707aa193aa7582bc68be11226f5de30add
Rishikeshari7 <rishikeshari7@gmail.com>
2023-12-02 02:54:48 +0530

    moved data code into a container with classNam-faculty-data-container

--- src/pages/fdashboard.jsx
+++ src/pages/fdashboard.jsx
@@ -37,18 +37,19 @@ function YourComponent() {
           <div className="user-info-div"></div>
           <div className="faculty-container">
             <h1>Faculty profile :</h1>
-            <p>FIRST NAME <strong>{dataa.first_name||"N/A"}</strong></p>
-            <p>LAST NAME <strong>{dataa.last_name||"N/A"}</strong></p>
-            <p>USER ID <strong>{dataa.user_id||"N/A"}</strong></p>
-            <p>EMAIL <strong>{dataa.email||"N/A"}</strong></p>
-            <p>PHONE NO. <strong>{dataa.phone_number||"N/A"}</strong></p>
-            <p>ROLE <strong>{dataa.role||"N/A"}</strong></p>
-            <p>POST <strong>{dataa.Post||"N/A"}</strong></p>
-            <p>DEPARTMENT <strong>{dataa.Department||"N/A"}</strong></p>
-            <p>AADHAR NO. <strong>{dataa.aadhar||"N/A"}</strong></p>
-            <p>GENDER <strong>{dataa.gender||"N/A"}</strong></p>
-            <p>ADDRESS <strong>{dataa.address||"N/A"}</strong></p>
-            
+              <div className="faculty-data-container">
+                  <p>FIRST NAME <strong>{dataa.first_name||"N/A"}</strong></p>
+                  <p>LAST NAME <strong>{dataa.last_name||"N/A"}</strong></p>
+                  <p>USER ID <strong>{dataa.user_id||"N/A"}</strong></p>
+                  <p>EMAIL <strong>{dataa.email||"N/A"}</strong></p>
+                  <p>PHONE NO. <strong>{dataa.phone_number||"N/A"}</strong></p>
+                  <p>ROLE <strong>{dataa.role||"N/A"}</strong></p>
+                  <p>POST <strong>{dataa.Post||"N/A"}</strong></p>
+                  <p>DEPARTMENT <strong>{dataa.Department||"N/A"}</strong></p>
+                  <p>AADHAR NO. <strong>{dataa.aadhar||"N/A"}</strong></p>
+                  <p>GENDER <strong>{dataa.gender||"N/A"}</strong></p>
+                  <p>ADDRESS <strong>{dataa.address||"N/A"}</strong></p>
+              </div>
           </div>
 
       </main>
