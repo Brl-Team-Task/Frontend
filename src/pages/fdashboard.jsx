d818dcad7996d22ae3d240a4d531daa23e3a2f5c
Rishikeshari7 <rishikeshari7@gmail.com>
2023-12-02 02:44:44 +0530

    printed the requied data,and return their values

--- src/pages/fdashboard.jsx
+++ src/pages/fdashboard.jsx
@@ -33,10 +33,25 @@ function YourComponent() {
     
   return (
     <>
-      <div>
-        <h1>Faculty</h1>
-        
-      </div>
+      <main className="wrapper">
+          <div className="user-info-div"></div>
+          <div className="faculty-container">
+            <h1>Faculty profile :</h1>
+            <p>FIRST NAME <strong>{dataa.first_name}</strong></p>
+            <p>LAST NAME <strong>{dataa.last_name}</strong></p>
+            <p>USER ID <strong>{dataa.user_id}</strong></p>
+            <p>EMAIL <strong>{dataa.email}</strong></p>
+            <p>PHONE NO. <strong>{dataa.phone_number}</strong></p>
+            <p>ROLE <strong>{dataa.role}</strong></p>
+            <p>POST <strong>{dataa.Post}</strong></p>
+            <p>DEPARTMENT <strong>{dataa.Department}</strong></p>
+            <p>AADHAR NO. <strong>{dataa.aadhar}</strong></p>
+            <p>GENDER <strong>{dataa.gender}</strong></p>
+            <p>ADDRESS <strong>{dataa.address}</strong></p>
+            
+          </div>
+
+      </main>
       
     </>
   );
