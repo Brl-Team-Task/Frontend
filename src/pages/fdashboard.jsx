0895327185390ac2f7616ecc3934ffb44ce1c438
Rishikeshari7 <rishikeshari7@gmail.com>
2023-12-02 02:47:45 +0530

    added the null value for the data value not present in api

--- src/pages/fdashboard.jsx
+++ src/pages/fdashboard.jsx
@@ -37,17 +37,17 @@ function YourComponent() {
           <div className="user-info-div"></div>
           <div className="faculty-container">
             <h1>Faculty profile :</h1>
-            <p>FIRST NAME <strong>{dataa.first_name}</strong></p>
-            <p>LAST NAME <strong>{dataa.last_name}</strong></p>
-            <p>USER ID <strong>{dataa.user_id}</strong></p>
-            <p>EMAIL <strong>{dataa.email}</strong></p>
-            <p>PHONE NO. <strong>{dataa.phone_number}</strong></p>
-            <p>ROLE <strong>{dataa.role}</strong></p>
-            <p>POST <strong>{dataa.Post}</strong></p>
-            <p>DEPARTMENT <strong>{dataa.Department}</strong></p>
-            <p>AADHAR NO. <strong>{dataa.aadhar}</strong></p>
-            <p>GENDER <strong>{dataa.gender}</strong></p>
-            <p>ADDRESS <strong>{dataa.address}</strong></p>
+            <p>FIRST NAME <strong>{dataa.first_name||"N/A"}</strong></p>
+            <p>LAST NAME <strong>{dataa.last_name||"N/A"}</strong></p>
+            <p>USER ID <strong>{dataa.user_id||"N/A"}</strong></p>
+            <p>EMAIL <strong>{dataa.email||"N/A"}</strong></p>
+            <p>PHONE NO. <strong>{dataa.phone_number||"N/A"}</strong></p>
+            <p>ROLE <strong>{dataa.role||"N/A"}</strong></p>
+            <p>POST <strong>{dataa.Post||"N/A"}</strong></p>
+            <p>DEPARTMENT <strong>{dataa.Department||"N/A"}</strong></p>
+            <p>AADHAR NO. <strong>{dataa.aadhar||"N/A"}</strong></p>
+            <p>GENDER <strong>{dataa.gender||"N/A"}</strong></p>
+            <p>ADDRESS <strong>{dataa.address||"N/A"}</strong></p>
             
           </div>
 
