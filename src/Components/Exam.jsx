d6dc26911ece37c1efa4d0c28e7b497e5fec07c7
Lucifer Locas <rohitsomvanshi2003@gmail.com>
2023-12-03 16:40:06 +0530

    corrected the exam fetch

--- src/Components/Exam.jsx
+++ src/Components/Exam.jsx
@@ -3,13 +3,12 @@ import axios from 'axios';
 import './exams.css';
 
 const API_URL = 'https://erp-backend-mqly.onrender.com/api/examdata/';
-const TOKEN = localStorage.getItem('Token');
+const token = localStorage.getItem('Token');
 
 
 const Exam = () => {
- const [token, setToken] = useState(TOKEN);
  const [data, setData] = useState([]); 
- const fetchData = async (token) => {
+ const fetchData = async () => {
    try {
      const response = await axios.get(API_URL, {
        headers: {
@@ -19,15 +18,14 @@ const Exam = () => {
  
      console.log('API Response:', response.data);
      setData(response.data.exam_data); 
-     console.log(data);
    } catch (error) {
      console.error('Error fetching data:', error.message);
    }
  };
 
   useEffect(() => {
-    fetchData(token);
-  }, [token]);
+    fetchData();
+  }, []);
 
   const getClassName = ({date}) => {
     const currentDate = new Date();
@@ -55,17 +53,17 @@ const Exam = () => {
       <table>
         <tbody>
         {data.map((item) => (
-<tr key={item.exam_data.id} className={getClassName(item.exam_data.date)}>
-              <td>{item.exam_data.exam_name}</td>
-              <td>{item.exam_data.total_marks}</td>
-              <td>{item.exam_data.date}</td>
+<tr key={item.id} className={getClassName(item.date)}>
+              <td>{item.exam_name}</td>
+              <td>{item.total_marks}</td>
+              <td>{item.date}</td>
               <td>
-                {item.exam_data.exam_name === 'University Exam' ? (
-                  <a href={item.exam_data.admit_card} download>
+                {item.exam_name === 'University Exam' ? (
+                  <a href={item.admit_card} download>
                     Admit Card
                   </a>
                 ) : (
-                  item.exam_data.duration
+                  item.duration
                 )}
               </td>
             </tr>
@@ -78,10 +76,10 @@ const Exam = () => {
       <table>
         <tbody>
         {data.map((item) => (
-<tr key={item.exam_data.id} className={getClassName(item.exam_data.date)}>
-              <td>{item.exam_data.exam_name}</td>
-              <td>{item.exam_data.total_marks}</td>
-              <td>{item.exam_data.duration}</td>
+<tr key={item.id} className={getClassName(item.date)}>
+              <td>{item.exam_name}</td>
+              <td>{item.total_marks}</td>
+              <td>{item.duration}</td>
             </tr>
           ))}
         </tbody>
@@ -92,11 +90,11 @@ const Exam = () => {
       <table>
         <tbody>
         {data.map((item) => (
-<tr key={item.exam_data.id} className={getClassName(item.exam_data.date)}>
-              <td>{item.exam_data.exam_name}</td>
-              <td>{item.exam_data.total_marks}</td>
+<tr key={item.id} className={getClassName(item.date)}>
+              <td>{item.exam_name}</td>
+              <td>{item.total_marks}</td>
               <td>
-                <a href={item.exam_data.result} download>
+                <a href={item.result} download>
                   Download Result
                 </a>
               </td>
